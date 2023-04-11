d2991f3136ba2c6d8f5f301c2d8038737a65f623
afrazahmad <afrazahmad313@gmail.com>
2023-04-11 12:34:05 +0500

    Created an api using contentfull, and fetch using Next.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  env: {
+    CONTENTFUL_SPACE_ID: "1rryfnptmjpp",
+    CONTENTFULL_ACCESS_KEY: "OBcMhYNrm825ezVNSGvHEb-RB9jAkmoY74eYQhZXUz4",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
